6d45068b5b71429dc1da777f1927b86e1958f4f7
MikeGPQ <miguelgarciaparra@hotmail.com>
2025-06-09 08:03:52 -0600

    First

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+      <div style={{
+          fontSize: '2.25rem',   
+          fontWeight: 'bold',     
+          color: '#3b82f6'       
+      }}>
+          Hello World
+      </div> 
+  )
+}
+
+export default App
